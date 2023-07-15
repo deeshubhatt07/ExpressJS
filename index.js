9154b6c2deb706461a4df64f7ab2307ea1a1f930
Deepanshu Bhatt <48888110+deeshubhatt07@users.noreply.github.com>
2023-07-16 00:36:40 +0530

    added post endpoint and parser

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/',(req,res) => {
     res.send('Hi Deepanshu');
+})
+
+app.get('/about',(req,res) => {
+    res.json({
+        name:"deeshu"
+    })
+})
+
+app.post('/',(req,res) => {
+    console.log(req);
+    res.json({
+        name:"deeshu"
+    })
 })
 
 app.listen(3000,()=>console.log('Port running at 3000'));
